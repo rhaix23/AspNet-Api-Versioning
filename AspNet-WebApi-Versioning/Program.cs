1de409beb8cbfbe1fd0762209334b6e28aa810f0
Patrick <patrick.lloyd.santos@gmail.com>
2024-03-22 03:02:37 +0800

    Versioning Setup

--- AspNet-WebApi-Versioning/Program.cs
+++ AspNet-WebApi-Versioning/Program.cs
@@ -1,5 +1,23 @@
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(2); // default version
+    options.ReportApiVersions = true; // report supported versions
+    options.AssumeDefaultVersionWhenUnspecified = true; // assume default version when not specified
+    options.ApiVersionReader = new UrlSegmentApiVersionReader(); // url segment versioning e.g., /api/v1.0/Values
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V"; // format for versioning
+    options.SubstituteApiVersionInUrl = true; // substitute version in url
+});
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.Run();
